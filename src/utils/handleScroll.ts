5a15f61a611db0e96a25fb104d1c5abb64e75ea1
minho <kasamin98@naver.com>
2024-12-01 20:57:13 +0900

    chore: 필요없는 주석 제거

--- src/utils/handleScroll.ts
+++ src/utils/handleScroll.ts
@@ -1,13 +1,13 @@
 const handleScroll = (targetId: string) => {
-  const target = document.querySelector(`#${targetId}`); // 명시적으로 HTMLElement로 타입 단언
+  const target = document.querySelector(`#${targetId}`);
 
   if (target instanceof HTMLElement) {
-    const targetPosition = target.offsetTop; // offsetTop 사용 가능
-    const offset = 50; // 50px 위로 이동
+    const targetPosition = target.offsetTop;
+    const offset = 50; // 고정 헤더만큼 이동
 
     window.scrollTo({
-      top: targetPosition - offset, // 목표 위치에서 50px 위로 조정
-      behavior: "smooth", // 부드러운 스크롤
+      top: targetPosition - offset,
+      behavior: "smooth",
     });
   }
 };
