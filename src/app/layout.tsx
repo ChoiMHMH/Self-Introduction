99ff7acaee81f4ed67f2571c19dd91ceaa47d100
minho <kasamin98@naver.com>
2024-12-04 02:25:21 +0900

    feat: React 18v로 변경 및 NextTopLoader설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import NextTopLoader from "nextjs-toploader";
 
 export const metadata: Metadata = {
   title: "최민혁 포트폴리오",
@@ -16,6 +17,7 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <body>
+        <NextTopLoader color="#9e24e6" />
         <div id="modal_root" />
         {children}
         {modal}
--- src/app/project/portfolio/page.tsx
+++ src/app/project/portfolio/page.tsx
@@ -4,15 +4,15 @@ import ProjectContainer from "@/components/projects/project_container";
 
 function Portfolio() {
   return (
-    <ProjectContainer title="Danchoo" subtitle="">
+    <ProjectContainer title="포트폴리오" subtitle="">
       <LeftImgItem
-        cardTitle="기본적인 최적화 및 반응형"
+        cardTitle="기본적인 최적화 및 반응형, 그리고 모달"
         cardDescription={[
           "상호작용하는 부분만 클라이언트로 제작",
           "Parallel & Intercepting Routes 를 사용하여 모달 제작 -> 뒤로가기 및 앞으로 가기 키로 제어 가능하고, URL로 매핑되면 검색 엔진에 해당 상태를 인덱싱하여 SEO 측면에서 이점이 있을 것으로 생각했습니다.",
           "pc 퍼스트로 Tablet , Mobile 반응형으로 제작",
         ]}
-        cardImage="/github.svg"
+        cardImage="/portfolio.png"
       ></LeftImgItem>
       <LeftImgItem
         cardTitle="SEO 설정"
