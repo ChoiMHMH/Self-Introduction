ba1659f95047091a507b7319a53bc1a26427bd13
minho <kasamin98@naver.com>
2024-12-04 12:23:26 +0900

    fit: skeleton이미지 지정 - 프로젝트의 이미지만

--- src/components/projects/left_img_item.tsx
+++ src/components/projects/left_img_item.tsx
@@ -1,4 +1,6 @@
+"use client";
 import Image from "next/image";
+import { useState } from "react";
 import styles from "./left_img_item.module.css";
 
 export type ModalCardItemProps = {
@@ -12,14 +14,20 @@ function LeftImgItem({
   cardDescription,
   cardImage,
 }: ModalCardItemProps) {
+  const [isImageLoading, setIsImageLoading] = useState(true);
+
   return (
     <div className={styles.card}>
+      {isImageLoading && <div className={styles.image_skeleton}></div>}
       <Image
-        className={styles.image_placeholder}
+        className={`${styles.image_placeholder} ${
+          isImageLoading ? styles.hidden : ""
+        }`}
         src={cardImage}
         alt={cardImage || "카드이미지"}
         width={400}
         height={400}
+        onLoad={() => setIsImageLoading(false)}
       />
       <div className={styles.text_container}>
         <h3 className={styles.card_title}>{cardTitle}</h3>
--- src/components/projects/right_img_item.tsx
+++ src/components/projects/right_img_item.tsx
@@ -1,20 +1,27 @@
+"use client";
 import Image from "next/image";
 import { ModalCardItemProps } from "./left_img_item";
 import styles from "./right_img_item.module.css";
+import { useState } from "react";
 
 function RightImgItem({
   cardTitle,
   cardDescription,
   cardImage,
 }: ModalCardItemProps) {
+  const [isImageLoading, setIsImageLoading] = useState(true);
   return (
     <div className={styles.card}>
+      {isImageLoading && <div className={styles.image_skeleton}></div>}
       <Image
-        className={styles.image_placeholder}
-        src={cardImage || ""}
+        className={`${styles.image_placeholder} ${
+          isImageLoading ? styles.hidden : ""
+        }`}
+        src={cardImage}
         alt={cardImage || "카드이미지"}
         width={400}
         height={400}
+        onLoad={() => setIsImageLoading(false)}
       />
       <div className={styles.text_container}>
         <h3 className={styles.card_title}>{cardTitle}</h3>
