dbdf6982f9e75ec3b363ba820fd3d332b342a469
minho <kasamin98@naver.com>
2024-12-07 23:53:01 +0900

    fix: sitemap수정

---
+++ src/app/sitemap.ts
@@ -0,0 +1,38 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = "https://cmh-portfolio.vercel.app/";
+
+  return [
+    {
+      url: `${baseUrl}/`,
+      lastModified: new Date(),
+      priority: 1.0,
+    },
+    {
+      url: `${baseUrl}/flawdetector`,
+      lastModified: new Date(),
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/portfolio`,
+      lastModified: new Date(),
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/danchoo`,
+      lastModified: new Date(),
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/mogazoa`,
+      lastModified: new Date(),
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/rolling`,
+      lastModified: new Date(),
+      priority: 0.8,
+    },
+  ];
+}
